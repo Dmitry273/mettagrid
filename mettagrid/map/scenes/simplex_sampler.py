84d92d2a00235a792fd790b009d6364d14128fe9
Catnee <ste01a01@gmail.com>
2025-04-20 05:15:01 +0200

    added 2 more generators, fixed pointed out issues, refactored to make code cleaner

--- mettagrid/map/scenes/simplex_sampler.py
+++ mettagrid/map/scenes/simplex_sampler.py
@@ -10,17 +10,29 @@ from mettagrid.map.utils.random import MaybeSeed
 
 import math
 
-def fn0(x,y, width, height, lx:float = 0.1, ly:float = 0.1, **args):
+'''
+This file contains a set of functions that can be used to generate different types of noise patterns for terrain generation.
+The functions are designed to be used with the OpenSimplex noise generator, and they can be combined in various ways to create complex terrain features.
+The functions take in parameters such as x and y coordinates, width and height of the terrain, and various other parameters that control the noise generation process.
+'''
+
+def fn0(x,y, width, height, 
+        lx:float = 0.1, 
+        ly:float = 0.1, 
+        ) -> tuple[float, float]:
+    # simple function to generate additional noise
     return (x*lx, y*ly)
 
-def fn1(x,y, width, height, **args):
+def fn1(x,y, width, height, **args) -> tuple[float, float]:
+    #currently unused
     octave = [0.1,0.1]
     xi = abs(x-0.5*y)*(x-0.5*width)*(y-0.5*height) * octave[0]
     yi = abs(x-0.5*y)*(x-0.5*width)*(y-0.5*height) * octave[1]
 
     return (xi, yi)
 
-def fn2(x,y, width, height, P, **args):
+def fn2(x,y, width, height, P, **args) -> tuple[float, float]:
+    # currently unused
     octave = [0.1,0.1]
     xi = (x-0.5*width)  * octave[0]
     yi = (y-0.5*height) * octave[1]
@@ -30,7 +42,8 @@ def fn2(x,y, width, height, P, **args):
 
     return (xi, yi)
 
-def fn3(x,y, width, height, **args):
+def fn3(x,y, width, height, **args) -> tuple[float, float]:
+    # currently unused
     octave = [0.1,0.1]
     if x%7 == 0 or y%3 == 0:
         xi, yi = 1*x, 4*y
@@ -39,37 +52,70 @@ def fn3(x,y, width, height, **args):
 
     return (xi*octave[0], yi*octave[1])
 
-def fn4(x, y, width, height, P, **args):
-    octave = [0.1,0.15]
-    xi = (x-0.5*width) * octave[0]
-    yi = (y-0.5*height) * octave[1]
-    a = P
-    xi, yi = np.sinc((xi**2+yi**2)*math.sin(math.atan(float(x/(y+0.01)))*2*math.pi/a)),np.sinc(xi**2+yi**2)*math.sin(math.atan(float(x/(y+0.01)))*2*math.pi/a)
-    return (xi, yi)
+def fn4(x, y, width, height,
+        lx:float = 0.1,
+        ly:float = 0.1, 
+        t:float = 0.25, 
+        x_pow:int = 2, 
+        y_pow:int = 2, 
+        xc:float = 0.0, 
+        yc:float = 0.0, 
+        P:float = 1.0, 
+        ax:float = 0.0,
+        ay:float = 0.0,
+        bx:float = 0.0,
+        by:float = 0.0,
+        ) -> tuple[float, float]:
+    # function used in "the_sphere" and "the_what" generator
+    xi = (x-(0.5+xc)*width)*0.05
+    yi = (y-(0.5+yc)*height)*0.05
+    alpha = 2*math.pi*t
+    cs = math.cos(alpha)
+    sn = math.sin(alpha)
+    xi, yi = (cs*(xi) + sn*(yi)), (-sn*(xi) + cs*(yi))
+    a = np.sinc(((bx+xi)**x_pow+(by+yi)**y_pow)*math.sin(math.atan2((y-(0.5+ay)*height),(x-(0.5+ax)*width))*P))
+    xi, yi = a, a
 
-def fn5(x,y, width, height, M, **args):
+    return (xi*lx, yi*ly)
+
+def fn5(x,y, width, height, M, **args) -> tuple[float, float]:
+    # currently unused
     octave = [0.5,0.5]
     xi = (x-M*width)**2 * octave[0]
     yi = (y-0.5*height)**2 * octave[1]
 
     return (xi, yi)
 
-def fn6(x,y, width, height, lx:float = 0.1, ly:float = 0.1, t:float = 0.25, x_pow:int = 2, y_pow:int = 2, flip:int = 1, **args):
-    octave = [lx,ly]
+def fn6(x,y, width, height, 
+        lx:float = 0.1, 
+        ly:float = 0.1, 
+        t:float = 0.25, 
+        x_pow:int = 2, 
+        y_pow:int = 2, 
+        xc:float = 0.0, 
+        yc:float = 0.0, 
+        ) -> tuple[float, float]:
+    # function used in "cross_curse" generator
     alpha = 2*math.pi*t
     cs = math.cos(alpha)
     sn = math.sin(alpha)
+    xi, yi = x, y
 
-    xi, yi = (cs*(x-0.5*width) + sn*(y-0.5*height)), (-sn*(x-0.5*width) + cs*(y-0.5*height))
+    xi, yi = (cs*(xi-(0.5+xc)*width) + sn*(yi-(0.5+yc)*height)), (-sn*(xi-(0.5+xc)*width) + cs*(yi-(0.5+yc)*height))
 
     xi, yi = 8 * xi**x_pow /x_pow**x_pow, 8 * yi**y_pow /y_pow**y_pow
-
-    xi, yi = (xi+0.5*width) * octave[0], (yi+0.5*height) * octave[1]
  
-    return (xi, yi)
+    return (xi*lx, yi*ly)
 
-def fn7(x,y, width, height, lx:float = 0.1, ly:float = 0.1, t:float = 0.25, symmetry:int = 3, xc:float = 0.1, yc:float = 0.3, **args):
-    octave = [lx,ly]
+def fn7(x,y, width, height, 
+        lx:float = 0.1, 
+        ly:float = 0.1, 
+        t:float = 0.25, 
+        symmetry:int = 3, 
+        xc:float = 0.0, 
+        yc:float = 0.0, 
+        ) -> tuple[float, float]:
+    # function used in "symmetry" generator
     alpha = 2*math.pi*t
     cs = math.cos(alpha)
     sn = math.sin(alpha)
@@ -82,17 +128,18 @@ def fn7(x,y, width, height, lx:float = 0.1, ly:float = 0.1, t:float = 0.25, symm
     xi, yi = (csb*(xi+a*width) - snb*(yi+b*height)), (snb*(xi+a*width) + csb*(yi+b*height))
 
     xi, yi = (cs*(xi-0.5*width+xc*width) - sn*(yi-0.5*height+yc*height)), (sn*(xi-0.5*width+xc*width) + cs*(yi-0.5*height+yc*height))
-    xi, yi = xi * octave[0], yi * octave[1]
  
-    return (xi, yi)
+    return (xi*lx, yi*ly)
 
 class Layer(NamedTuple):
     fn: 'function'
     saturation: float
     params: dict[str,Any]
 
-class TerrainGen(Scene):
+class SimplexSampler(Scene):
+
     EMPTY, WALL = "empty", "wall"
+
     def __init__(
         self,
         room_size: int = 1,
@@ -100,44 +147,37 @@ class TerrainGen(Scene):
         seed: MaybeSeed = None,
         children: list[Any] = [],
         layers: list[Layer] = [], # layers dictate how generated noise is sampled and how the end result will look
-        sampling_params: dict = {}, # additional optional non-necessary helping params, can help with creating interpolated sets of rooms
         cutoff: int = 70,
     ):
         super().__init__(children=children)
         self._room_size = room_size
         self._wall_size = wall_size
         self.seed = seed
-        self.sampling_params = sampling_params
         self.cutoff = cutoff
         self._rng = np.random.default_rng(seed)
         self.layers = [Layer(hydra.utils.get_method(x.fn), x.saturation, x.params) for x in layers if isinstance(x.fn, str)]
         
-
-
-    def _render(self, node: Node):
+    def _render(self, node: Node) -> None:
         grid = node.grid
-        self._width = node.width
-        self._height = node.height
+        self._height, self._width = node.grid.shape
         
-        terrain = np.ones(shape=(self._width,self._height)) # template neutral terrain of ones to prevent errors
+        terrain = np.ones(shape=(grid.shape)) # template neutral terrain of ones to prevent errors
         for layer in self.layers:
-            if abs(layer.saturation) > 0.00001: terrain *= self.terrain_map(layer) # terrains layered on top of each other via multiplication,
-                                                                              # terrains with ~0 saturation are skipped from calculation
-        terrain = self.normalize_array(terrain) # sets min value as 0, max as 1 via appropriate rescaling
+            if abs(layer.saturation) > 0.00001: terrain *= self.terrain_map(layer)      # terrains layered on top of each other via multiplication,
+                                                                                        # terrains with ~0 saturation are skipped from calculation
+        terrain = self.normalize_array(terrain)                                         # sets min value as 0, max as 1 via appropriate rescaling
         terrain = (terrain * 255).astype('uint8')
-        # add converters and stuff
+
         room = np.array(np.where(terrain > self.cutoff, self.EMPTY,self.WALL), dtype='<U50')
-        self.fix_map(room) # function that finds unconnected regions and connects them anyway, works slow, could be optimized
-        
-        grid[:] = room.transpose()
-        return room
+        self.fix_map(room)
+        grid[:] = room
     
     def normalize_array(self, room: np.ndarray) -> np.ndarray:
         norm_arr = (room - np.min(room)) / (np.max(room) - np.min(room))
         return norm_arr
     
     def terrain_map(self, layer: Layer) -> np.ndarray:
-        simplex = OpenSimplex(self._rng.integers(0, 2**15-1))
+        simplex = OpenSimplex(self._rng.integers(0, 2**31-1))
 
         xa = np.array(range(self._width))
         ya = np.array(range(self._height))
@@ -146,7 +186,6 @@ class TerrainGen(Scene):
                                                                                                               # the slower noise is sampled: fn(x,y) !=fn(x+1,y) in some region the more changes will be in this region per pixel, noise will look zoomed out
         terrain = (terrain + 1)/2 # changes range from [-1,1] to [0,1]
         terrain = terrain.reshape((self._height,self._width))
-        terrain = terrain.transpose()
         terrain = terrain**layer.saturation # saturates pattern with walls. Helpful since base noise is balanced to be 50/50. Saturation 0 makes neutral terrain with 0 walls, saturation >100 fills everything with walls.
 
         return terrain
@@ -158,7 +197,7 @@ class TerrainGen(Scene):
             return
         start = (start[0][0],start[1][0])
 
-        def out_of_bound(x, y):
+        def out_of_bound(y, x):
             if x < 0 or y < 0:
                 return True
             if x >= self._width or y >= self._height:
---
+++ mettagrid/map/tests/mapgen_manual_drawing.py
@@ -0,0 +1,48 @@
+from mettagrid.map.mapgen import MapGen
+from omegaconf import OmegaConf
+import numpy as np
+from mettagrid.resolvers import register_resolvers
+
+from PIL import Image
+import glob
+import os
+
+w = 180
+h = 180
+
+for i in range(32):
+    register_resolvers()
+    config = OmegaConf.load('configs/game/map_builder/mapgen_simsam_cross_curse.yaml')
+
+    if OmegaConf.select(config, "root") is not None:
+        root = config.root
+    else:
+        print("No root config found, using default maze")
+        
+    world_map = MapGen(w,h,root = root).build()
+
+    Image.fromarray((world_map == 'empty')).resize((400, 400*h//w)).save(f'{i}.png')
+
+
+'''
+This code generates a gif from the set of generated maps
+additional requirements: pip install pillow
+'''
+
+# def make_gif():
+#     frames = [Image.open(image) for image in sorted(glob.glob('/home/catnee/mettagrid/giffactory/*.png'), key=os.path.getmtime)]
+#     frame_one = frames[0]
+#     frame_one.save("my_awesome.gif", format="GIF", append_images=frames,
+#                save_all=True, duration=100, loop=0)
+
+# N = 100
+# for i in range(N):
+#     print(f'{i} step out of {N}')
+#     t = 0.01*(1.08**i)
+#     m = 0.01*i
+#     room = TerrainGen(w,h, layers, sampling_params={'M':t, 'O':m}, scenario='maze', seed=11)._build()
+#     room = (room != 'wall')
+#     Image.fromarray(room).resize((400, 400)).save(f'giffactory/{i}.png')
+
+# make_gif()
+# print(f'{N} step out of {N}')
